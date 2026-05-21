4f81c0c58c748040d87ab2c61762c53d1243f6fd
Walid Regaia <walidregaia@Walids-MacBook-Pro.local>
2026-05-21 11:35:05 +0400

    final: remove adapter clean setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
   site: 'https://dein-beauty-kurs.de'
 });
