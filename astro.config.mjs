3649f7e289ff3ac3c79225fc24064a8fa0971de8
BeautyBusiness360 <info@beauty-360.de>
2026-06-12 16:40:33 +0400

    config: /berlin/ Redirect fix mit trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   },
   redirects: {
     '/berlin':             { status: 301, destination: '/' },
+    '/berlin/':            { status: 301, destination: '/' },
     '/luxbeauty':          { status: 301, destination: '/' },
     '/info-veranstaltung': { status: 301, destination: '/' },
     '/train-the-trainer':  { status: 301, destination: '/' },
